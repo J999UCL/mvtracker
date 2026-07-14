Fix scene rotation tensor conversion

mvtracker/datasets/kubric_multiview_dataset.py
@@ -1034,9 +1034,9 @@ class KubricMultiViewDataset(torch.utils.data.Dataset):
             rot_x = R.from_euler('x', rot_x_angle, degrees=True).as_matrix()
             rot_y = R.from_euler('y', rot_y_angle, degrees=True).as_matrix()
             rot_z = R.from_euler('z', rot_z_angle, degrees=True).as_matrix()
-            rot = rot_z @ rot_y @ rot_x
+            rot = torch.from_numpy(rot_z @ rot_y @ rot_x)
             T_rot = torch.eye(4)
-            T_rot[:3, :3] = torch.from_numpy(rot)
+            T_rot[:3, :3] = rot
             T_scale_and_translate = torch.tensor([
                 [scale, 0.0, 0.0, translate_x],
                 [0.0, scale, 0.0, translate_y],